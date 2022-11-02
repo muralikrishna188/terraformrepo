aada156c8e2afdef087111b80e0332623257e127
muralikrishna188 <muralid188@outlook.com>
2022-11-02 08:31:44 +0530

    update

--- main.tf
+++ main.tf
@@ -98,31 +98,7 @@ resource "azurerm_network_interface" "tflabelwebnic101" {
   }
   
 }
-resource "azurerm_linux_virtual_machine" "tflabelwebserver101" {
-  name = "tfwebserver"
-  resource_group_name = azurerm_resource_group.tflabelrg101.name
-  location = azurerm_resource_group.tflabelrg101.location
-  size = "Standard_F2"
-  admin_username = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.tflabelwebnic101.id,
-  ]
-  admin_ssh_key {
-    username = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-  os_disk {
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "18.04-LTS"
-    version = "latest"
-  }
-  
-}
+
 
 resource "azurerm_public_ip" "tflabelapppublicip101" {
   name = "tfapppublicip"
@@ -145,28 +121,3 @@ resource "azurerm_network_interface" "tflabelappnic101" {
   
 }
 
-resource "azurerm_linux_virtual_machine" "tflabelappserver101" {
-  name = "tfappserver"
-  resource_group_name = azurerm_resource_group.tflabelrg101.name
-  location = azurerm_resource_group.tflabelrg101.location
-  size = "Standard_F2"
-  admin_username = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.tflabelappnic101.id,
-  ]
-  admin_ssh_key {
-    username = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-  os_disk {
-    caching = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "18.04-LTS"
-    version = "latest"
-  }
-  
-}
